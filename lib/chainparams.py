d4fb69be38fa3dea7c77e4df4df9dad5bee4232c
Kefkius <Kefkius@users.noreply.github.com>
2015-03-30 16:57:50 -0400

    Fix tests more
    Fix tests a little more

--- lib/chainparams.py
+++ lib/chainparams.py
@@ -91,10 +91,9 @@ def get_chain_instance(code):
     if not is_known_chain(code): return None
     params = get_params(code)
     module_name = params.module_name
-    classmodule = importlib.import_module(''.join(['chainkey.chains.', module_name]))
-    # If nothing was imported, try with a different import path
-    # Note that this is the path nosetests use, and so this is necessary
+    # If importing fails, try with a different path.
     try:
+        classmodule = importlib.import_module(''.join(['chainkey.chains.', module_name]))
         classInst = getattr(classmodule, 'Currency')
     except AttributeError:
         classmodule = importlib.import_module(''.join(['lib.chains.', module_name]))
