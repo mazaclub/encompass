972728f20cf85df989ac40b90fb7f17ce7f6e239
Kefkius <kefkius@mail.com>
2015-06-26 16:35:01 -0400

    Use a Form Layout for Transaction dialog

--- plugins/clamspeech.py
+++ plugins/clamspeech.py
@@ -8,11 +8,16 @@ class Plugin(BasePlugin):
 
     @hook
     def transaction_dialog(self, d):
-        clamspeech = getattr(d.tx, 'clamspeech', '')
-        vbox = d.layout()
-        vbox.addWidget(QLabel(_("ClamSpeech")))
+        clamspeech = getattr(d.tx, 'clamspeech', None)
+        if not clamspeech:
+            return
+        form = d.layout()
+        vbox = QVBoxLayout()
+        vbox.addWidget(QLabel(_("ClamSpeech:")))
         speech_text = QTextEdit()
         speech_text.setReadOnly(True)
         speech_text.setMaximumHeight(100)
         speech_text.setText(clamspeech)
+        speech_text.setFixedHeight(50)
         vbox.addWidget(speech_text)
+        form.insertRow(form.rowCount() - 1, vbox)
