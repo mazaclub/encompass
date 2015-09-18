bd326b7aba0d8100dfca94d669e7d6c20683f610
Rob Nelson <guruvan@maza.club>
2015-09-18 16:20:49 -0700

    get travis to move or cp to releases dir

--- contrib/mazaclub-release/helpers/build_release.sh
+++ contrib/mazaclub-release/helpers/build_release.sh
@@ -6,7 +6,7 @@ source helpers/build-common.sh
 check_vars
 $DOCKERBIN run --rm -it --privileged -e MKPKG_VER=${VERSION} -v $(pwd)/releases:/releases -v $(pwd)/helpers:/root  -v $(pwd)/repo:/root/repo  -v $(pwd)/source:/opt/wine-electrum/drive_c/encompass/ -v $(pwd):/root/encompass-release mazaclub/encompass-release:${VERSION} /root/make_release $VERSION $TYPE 
 test -d releases/ || mkdir releases
-if [ -z  ${TRAVIS_BUILD_DIR} ] ; then 
+if [ -d  ${TRAVIS_BUILD_DIR} ] ; then 
   sudo chown -R $(whoami) releases
   cp -av helpers/release-packages/Source releases/
 else 
