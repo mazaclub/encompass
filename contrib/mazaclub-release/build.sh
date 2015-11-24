5b75839becbf7abc40a1c1061e88e53effb31766
Rob Nelson <guruvan@maza.club>
2015-11-24 12:15:29 -0800

    Add build info to signed release, don't push rc docker images

--- contrib/mazaclub-release/build.sh
+++ contrib/mazaclub-release/build.sh
@@ -24,4 +24,31 @@ fi
 # move completed builds from helpers/release-packages to releases/
 # sum and sign the binaries, zipfiles, and tarballs
 completeReleasePackage ${OS}
+mv Encompass-${VERSION}.sums releases/
+# this is done here so we don't run this by hand on manual builds
+# get all git URI/commits for build system & product 
+# show config & build command line
+# add it all to the .sums file and gpg sign it
+echo "BUILD COMMAND: ${BUILD_CMD}"  >> releases/Encompass-${VERSION}.sums
+echo "BUILD SYSTEM ==================="  >> releases/Encompass-${VERSION}.sums
+echo "Built with $(grep -i url .git/config)" >> releases/Encompass-${VERSION}.sums
+echo "Build system commit: $(git rev-parse HEAD  --short)" >>  releases/Encompass-${VERSION}.sums
+echo "git diff: $(git diff)" >>  releases/Encompass-${VERSION}.sums
+echo "PRODUCT CODE ===================" >>  releases/Encompass-${VERSION}.sums
+echo "Built from $(grep -i url repo/.git/config)" >> releases/Encompass-${VERSION}.sums
+echo "Built from commit $(cd repo ; git rev-parse HEAD  --short)" >>  releases/Encompass-${VERSION}.sums
+echo "git diff: $(cd repo ; git diff)" >>  releases/Encompass-${VERSION}.sums
+echo "BUILD CONFIG ==================="  >> releases/Encompass-${VERSION}.sums
+echo "build-config.sh:" >> releases/Encompass-${VERSION}.sums
+echo " " >> releases/Encompass-${VERSION}.sums
+cat helpers/build-config.sh >> releases/Encompass-${VERSION}.sums
+### sign the sums file if this is a public release
+if [ "${TYPE}" = "rc" ] ; then
+   export TYPE="SIGNED"
+fi
+if [ "${TYPE}" = "SIGNED" ] ; then 
+   gpg --output releases/Encompass-${VERSION}.sums.asc --sign -armor --detach  releases/Encompass-${VERSION}.sums
+fi
+
+
 echo "End."
--- contrib/mazaclub-release/helpers/build-common.sh
+++ contrib/mazaclub-release/helpers/build-common.sh
@@ -116,30 +116,7 @@ build_Linux(){
 }
 completeReleasePackage(){
 #  mv $(pwd)/helpers/release-packages/* $(pwd)/releases/
-  if [ "${TYPE}" = "rc" ]; then export TYPE=SIGNED ; fi
-  if [ "${TYPE}" = "SIGNED" ] ; then
-    ${DOCKERBIN} push mazaclub/encompass-winbuild:${VERSION}
-    ${DOCKERBIN} push mazaclub/encompass-release:${VERSION}
-    ${DOCKERBIN} push mazaclub/encompass32-release:${VERSION}
-    ${DOCKERBIN} tag -f ogrisel/python-winbuilder mazaclub/python-winbuilder:${VERSION}
-    ${DOCKERBIN} push mazaclub/python-winbuilder:${VERSION}
-    cd releases
-    for release in * 
-    do
-      if [ ! -d ${release} ]; then
-         sign_release ${release}
-      else
-         cd ${release}
-         for i in * 
-         do 
-           if [ ! -d ${i} ]; then
-              sign_release ${i}
-	   fi
-         done
-         cd ..
-      fi
-    done
-  fi
+  ./helpers/complete_release_package.sh
   echo "You can find your Encompasss $VERSION binaries in the releases folder."
   
 }
