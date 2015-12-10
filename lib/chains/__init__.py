05a3b224af2e5d1ded168a775402d1eba829a6dc
Kefkius <kefkius@mail.com>
2015-12-09 19:15:18 -0500

    Require currency modules to load

--- lib/chains/__init__.py
+++ lib/chains/__init__.py
@@ -1,41 +1,44 @@
-import os, sys
+import os, sys, traceback
 
 
-if getattr(sys, 'frozen', False) and sys.platform=='darwin':
-      import chains.cryptocur
-      from chains.cryptocur import chainhook
-else: 
-      import cryptocur
-      from cryptocur import chainhook
-if getattr(sys, 'frozen', False):
-      import chains.bitcoin_chainkey
-      import chains.mazacoin
-      import chains.clam
-      import chains.litecoin
-      import chains.viacoin
-      import chains.dash
-      import chains.peercoin
-      import chains.dogecoin
-      import chains.blackcoin
-      import chains.feathercoin
-      import chains.groestlcoin
-      import chains.namecoin
-      import chains.startcoin
+try:
+    if getattr(sys, 'frozen', False) and sys.platform=='darwin':
+          from chains import cryptocur
+    else:
+          import cryptocur
+    if getattr(sys, 'frozen', False):
+          from chains import bitcoin_chainkey
+          from chains import mazacoin
+          from chains import clam
+          from chains import litecoin
+          from chains import viacoin
+          from chains import dash
+          from chains import peercoin
+          from chains import dogecoin
+          from chains import blackcoin
+          from chains import feathercoin
+          from chains import groestlcoin
+          from chains import namecoin
+          from chains import startcoin
 
-else:
-      import bitcoin_chainkey
-      import mazacoin
-      import clam
-      import litecoin
-      import viacoin
-      import dash
-      import peercoin
-      import dogecoin
-      import blackcoin
-      import feathercoin
-      import groestlcoin
-      import namecoin
-      import startcoin
+    else:
+          import bitcoin_chainkey
+          import mazacoin
+          import clam
+          import litecoin
+          import viacoin
+          import dash
+          import peercoin
+          import dogecoin
+          import blackcoin
+          import feathercoin
+          import groestlcoin
+          import namecoin
+          import startcoin
+except Exception:
+    traceback.print_exc(file=sys.stdout)
+    print('ERROR: Cannot load modules for cryptocurrencies.')
+    sys.exit(1)
 
 
 # Thrown when an interface serves a header
